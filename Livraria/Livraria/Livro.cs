c87a41b2afc2cb7d97bc594cfe848038af3a808f
UrsulaRosa <ursularosa13@gmail.com>
2016-07-26 00:01:15 -0300

    Livraria programa

---
+++ Livraria/Livraria/Livro.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    class Livro
+    {
+         private List<Livro> Livros = new List<Livro>();
+        public string nomeLivro { get; set; }
+        public int id_Livro { get; set; }
+        public string genero { get; set; }
+        public string autor { get; set; }
+        public int quantidade { get; set; }
+        public double preco { get; set; }
+
+        //public void Livro()
+        //{
+        //    this.Livros = new List<Livro>();
+        //}
+        public void Estoque(Livro livro)
+        {
+            this.Livros.Add(livro);
+        }
+        public void Cadastrar()
+        {
+            Console.Clear();
+            Console.Write("Nome: ");
+            nomeLivro = Console.ReadLine();
+            Console.Write("ISBN: ");
+            id_Livro = int.Parse(Console.ReadLine());
+            Console.Write("Autor: ");
+            autor = Console.ReadLine();
+            Console.Write("Gênero: ");
+            genero = Console.ReadLine();
+            Console.Write("Quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Preço: ");
+            preco = double.Parse(Console.ReadLine());
+        }
+        public void Consultar()
+        {
+
+        }
+        public void Alterar()
+        {
+
+        }
+        public void Remover()
+        {
+
+        }
+        public void Sair ()
+        {
+            
+        }
+    }
+}
+
+   
+
---
+++ Livraria/Livraria/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Menu: \n 1. Cadastrar \n 2. Consultar \n 3. Alterar \n 4. Remover \n 5. Sair \n ----------- ");
+            int menu = int.Parse(Console.ReadLine());
+            Livro l = new Livro();
+            if (menu == 1)
+                l.Cadastrar();
+            if (menu == 2)
+                l.Consultar();
+            if (menu == 3)
+                l.Alterar();
+            if (menu == 4)
+                l.Remover();
+            if (menu == 5)
+                Environment.Exit(menu);
+        }
+    }
+}
